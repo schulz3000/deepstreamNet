63c2c64f94e5f573231d536dcb2aba11b1fb0db7
Xaver Schulz <schulz3000@gmx.de>
2016-06-02 22:26:05 +0200

    Fix TestClient

--- src/DeepStreamTestClient/Program.cs
+++ src/DeepStreamTestClient/Program.cs
@@ -21,7 +21,7 @@ namespace DeepStreamNet.ConsoleClient
         async static Task Exec()
         {
             var client = new DeepStreamClient("localhost", 6021);
-                        
+
             if (await client.LoginAsync())
             {
                 var disp = await client.Events.Subscribe("test", Console.WriteLine);
@@ -30,11 +30,11 @@ namespace DeepStreamNet.ConsoleClient
 
                 await client.Events.PublishAsync("test", "Hello World");
 
-                await Task.Delay(30000);               
+                await Task.Delay(30000);
 
                 Console.ReadKey();
-               
-                disp.Dispose();
+
+                await disp.DisposeAsync();
             }
 
             client.Dispose();
