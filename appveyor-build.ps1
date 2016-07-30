f4009a1b308c8fe82eed99a0b613f31627acc4e3
Xaver Schulz <schulz3000@gmx.de>
2016-07-30 16:28:07 +0200

    try fixing appveyor CI build 2. try

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -56,7 +56,7 @@ EnsurePsbuildInstalled
 
 exec { & dotnet restore }
 
-Invoke-MSBuild
+Invoke-MSBuild .\src\DeepStreamNet.sln
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
