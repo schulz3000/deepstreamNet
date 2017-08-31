1f9d3813d3899b0566fa62b6019266f779d8ee42
Xaver Schulz <schulz3000@gmx.de>
2017-08-31 20:32:18 +0200

    Add Test for Presence Feature

---
+++ src/DeepStreamNet.Tests/PresenceTests.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeepStreamNet.Tests
+{
+    [Collection("ServerCommunication")]
+    public class PresenceTests : IClassFixture<DeepStreamServerFixture>
+    {
+        public PresenceTests(DeepStreamServerFixture fixture)
+        {
+            fixture.StartServer();
+        }
+
+        [Fact]
+        public async Task GetAllEmptyTest()
+        {
+            using (var client = await TestHelper.GetClient())
+            {
+              var users = await  client.Presence.GetAllAsync();
+                Assert.Empty(users);
+            }
+        }
+    }
+}
