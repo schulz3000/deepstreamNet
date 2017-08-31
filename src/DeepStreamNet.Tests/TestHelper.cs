556fc8cfaf59f754c0a71cdc9227342fe28ad13c
Xaver Schulz <schulz3000@gmx.de>
2017-08-31 22:00:49 +0200

    Try to enable Tests on Travis-CI

--- src/DeepStreamNet.Tests/TestHelper.cs
+++ src/DeepStreamNet.Tests/TestHelper.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
 using System.Threading.Tasks;
 
 namespace DeepStreamNet.Tests
@@ -8,9 +7,6 @@ namespace DeepStreamNet.Tests
     {
         public static async Task<DeepStreamClient> GetClient()
         {
-            Console.WriteLine("Custom output");
-            Console.WriteLine(Config["deepStreamHost"]);
-            Console.WriteLine(Config["ds-deepStreamHost"]);
             var client = new DeepStreamClient(Config["deepStreamHost"], int.Parse(Config["deepStreamPort"]), Config["deepStreamPath"], bool.Parse(Config["useSecureConnection"]));
             await client.LoginAsync();
             return client;
